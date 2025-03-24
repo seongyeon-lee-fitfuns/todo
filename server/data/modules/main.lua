8c9f8538b74f720b01dd516d9dbf047b429e0eb9
seongyeon-lee-fitfuns <sy.lee@fitfuns.com>
2025-03-24 16:37:16 +0900

    feat: 서버 설정 파일 추가 및 Lua 모듈 수정

--- server/data/modules/main.lua
+++ server/data/modules/main.lua
@@ -3,17 +3,15 @@ local nk = require("nakama")
 nk.logger_info("Hello, world!")
 
 nk.register_rpc(function(context, payload)
-    return "Hello, world! This is the my first lua rpc"
-end, "hello")
+    local user_id = context.user_id
+    local storage_object = nk.storage_read(user_id, "user_data")
+    nk.logger_info("Storage object: " .. storage_object)
+    nk.logger_info("Payload: " .. payload)
+    return payload
+end, "ReadStorageObject")
 
 nk.register_req_before(function(context, payload)
-    nk.logger_info("Before AddFriends")
+    nk.logger_info("Before ")
     return payload
-end, "AddFriends")
-
-nk.register_req_after(function(context, payload)
-    nk.logger_info("After AddFriends")
-    return payload
-end, "AddFriends")
-
+end, "hello")
 
